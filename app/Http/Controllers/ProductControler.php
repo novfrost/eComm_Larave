a5a02c133ce0cd5732eaf9724f8670e0ebfd6cff
novfrost <cvillegasfrola@gmail.com>
2020-12-17 17:19:29 -0600

    Laravel E-commerce Project #5 Login with session complete

---
+++ app/Http/Controllers/ProductControler.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductControler extends Controller
+{
+    //
+    function index() {
+        return "Welcome to Product Page";
+    }
+}
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,14 +1,14 @@
 @extends('master')
 @section('content')
-<form class="form-signin">
-
+<form class="form-signin" action="login" method="POST">
+    @csrf
   <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
   <label for="inputEmail" class="sr-only">Email address</label>
-  <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+  <input type="email" name="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
   <label for="inputPassword" class="sr-only">Password</label>
-  <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+  <input type="password" name="password" id="inputPassword" class="form-control" placeholder="Password" required>
 
-  <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+  <button class="btn btn-lg btn-primary btn-block" type="submit">Login</button>
 </form>
     
 @endsection
