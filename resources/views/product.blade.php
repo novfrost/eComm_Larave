7a6eb62ac0bcca3c1df6582629edba6226a91a31
novfrost <cvillegasfrola@gmail.com>
2020-12-18 12:24:41 -0600

    Laravel E-commerce Project #8 Home Page complete with Trending Product List

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -1,27 +1,44 @@
 @extends('master')
 @section('content')
 
-<div class="container-fluid">
-    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-    <div class="carousel-inner">
-        
-        @foreach ($products as $item)
+    <div class="row">
+        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+                
+                @foreach ($products as $item)
 
-        <div class="carousel-item {{$item['id']==1?'active':''}}">
-            <img class="d-block w-100 slider-img" src="{{$item['gallery']}}">
+                <div class="carousel-item {{$item['id']==1?'active':''}}">
+                    <img class="d-block w-100 slider-img" src="{{$item['gallery']}}">
+                </div>
+
+                @endforeach
+            </div>
+
+            <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+
+            <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
         </div>
+    </div>
 
-        @endforeach
+    <div class="row">
+        <div class="trending-wrapper">
+            <h3>Trending Products</h3>
+
+            @foreach ($products as $item)
+
+            <div>
+                <img class="d-block w-100 slider-img" src="{{$item['gallery']}}">
+            </div>
+
+            @endforeach
+        </div>
     </div>
-    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-    </a>
-    <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-    </a>
-    </div>
-</div>
+    
     
 @endsection
